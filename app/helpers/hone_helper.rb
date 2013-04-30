a68f87fa8e1916d6098508ec20d779199d71070c
Nitesh <engineernitesh@gmail.com>
2013-04-30 14:49:55 +0200

    Initial Commit

---
+++ app/helpers/hone_helper.rb
@@ -0,0 +1,2 @@
+module HoneHelper
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeoTest::Application.config.secret_token = '8f9cd22db0435f884fd5f47c2b45df017639f95ad43393ee79a5f3aab2ea0546872222bd6c9df529b0c6201f5b64acd2726a936404b19114d8602a864a8e20a7'
